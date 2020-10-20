4080cbdcd3e8ea54241218ee7fe70c731223ea7f
Alicia Fiuza <aliciafiuzaa@gmail.com>
2020-10-20 19:53:18 +0100

    Created seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+puts "users destroyed"
+
+user = User.create(first_name: "john", last_name: "smith", email: "john@dot.com", password: "123456")
+user1 = User.create(first_name: "ana", last_name: "smith", email: "ana@dot.com", password: "123456")
+user2 = User.create(first_name: "marjorie", last_name: "smith", email: "marjorie@dot.com", password: "123456")
+user3 = User.create(first_name: "claudine", last_name: "smith", email: "claudine@dot.com", password: "123456")
+user4 = User.create(first_name: "alicia", last_name: "smith", email: "alicia@dot.com", password: "123456")
+puts "users created"
+
+
+trait = Trait.create(name: "competence", description: "lorem ipsum", price_hour: 10)
+trait1 = Trait.create(name: "warmth", description: "lorem ipsum", price_hour: 15)
+trait2 = Trait.create(name: "outgoing", description: "lorem ipsum", price_hour: 20)
+trait3 = Trait.create(name: "honest", description: "lorem ipsum", price_hour: 25)
+trait4 = Trait.create(name: "trustworthy", description: "lorem ipsum", price_hour: 30)
+
+puts "traits created"
