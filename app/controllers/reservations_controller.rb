9faa0c5fc659ed86803fdbbe14e12597049bda12
ana sanots <anasoliveiraaa@gmail.com>
2020-10-23 19:00:30 +0100

    fixed reservation controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,12 +1,12 @@
 class ReservationsController < ApplicationController
+# before_action :find_trait, only: [:show, :new, :create, :edit, :destroy]
+
 before_action :find_reservation, only: [:show, :edit, :destroy]
-before_action :find_trait, only: [:show,:new, :edit, :destroy]
 
   def index
     # show all the reservations to all the user?
     # show all the reservations for a specific user?
     @reservations = Reservation.all
-
   end
 
   def show
@@ -15,6 +15,7 @@ before_action :find_trait, only: [:show,:new, :edit, :destroy]
   end
 
   def new
+    @trait = Trait.find(params[:trait_id])
     @reservation = Reservation.new
   end
 
@@ -23,32 +24,38 @@ before_action :find_trait, only: [:show,:new, :edit, :destroy]
     @trait = Trait.find(params[:trait_id])
     @reservation = Reservation.new(reservation_params)
     @reservation.trait = @trait
-    # @reservation.user = current_user
+    @reservation.user = current_user
 
     if @reservation.save
-      redirect_to reservation_path(@reservation), notice: 'Reservation successful' # or uccess page?
+      redirect_to trait_reservations_path(@trait, @reservation)
     else
       # @trait = Trait.find(params[:trait_id])
-      render "traits/show"
+      render :new
     end
   end
 
+  def edit;end
+
   def update
-    @reservation = Reservation.update(reservation_params)
+      if @reservation.updat(reservation_params)
+      redirect_to reservations_path
+    else
+      render :new
+    end
   end
 
   def destroy
     @reservation.destroy
-    redirect_to trait_path(@trait), notice: 'Reservation deleted.'
+    redirect_to trait_reservations_path(@reservation)
   end
 
 
 
   private
 
-  def find_trait
-     @trait = Trait.find(params[:trait_id])
-  end
+  # def find_trait
+  #    @trait = Trait.find(params[:trait_id])
+  # end
 
   def find_reservation
     @reservation = Reservation.find(params[:id])
