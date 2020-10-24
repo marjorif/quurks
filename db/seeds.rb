be1884062e7012cad0ba27e5ff0cd384e0e545a2
Marjori Fonte <marjorif@live.com>
2020-10-24 14:29:29 +0100

    Working on traits detail and updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,19 +10,19 @@ User.destroy_all
 puts "users destroyed"
 Trait.destroy_all
 
-user = User.create!(first_name: "john", last_name: "smith", email: "john@dot.com", password: "123456")
-user1 = User.create!(first_name: "ana", last_name: "smith", email: "ana@dot.com", password: "123456")
-user2 = User.create!(first_name: "marjorie", last_name: "smith", email: "marjorie@dot.com", password: "123456")
-user3 = User.create!(first_name: "claudine", last_name: "smith", email: "claudine@dot.com", password: "123456")
-user4 = User.create!(first_name: "alicia", last_name: "smith", email: "alicia@dot.com", password: "123456")
+user = User.create!(first_name: "John", last_name: "Smith", email: "john@dot.com", password: "123456")
+user1 = User.create!(first_name: "Ana", last_name: "Smith", email: "ana@dot.com", password: "123456")
+user2 = User.create!(first_name: "Marjori", last_name: "Smith", email: "marjorie@dot.com", password: "123456")
+user3 = User.create!(first_name: "Claudine", last_name: "Smith", email: "claudine@dot.com", password: "123456")
+user4 = User.create!(first_name: "Alicia", last_name: "Smith", email: "alicia@dot.com", password: "123456")
 puts "users created"
 
 
-trait = Trait.create!(name: "competence", description: "lorem ipsum", price_hour: 10, user: user)
-trait1 = Trait.create!(name: "warmth", description: "lorem ipsum", price_hour: 15, user: user1)
-trait2 = Trait.create!(name: "outgoing", description: "lorem ipsum", price_hour: 20, user: user2)
-trait3 = Trait.create!(name: "honest", description: "lorem ipsum", price_hour: 25, user: user3)
-trait4 = Trait.create!(name: "trustworthy", description: "lorem ipsum", price_hour: 30, user: user4)
+trait = Trait.create!(name: "Competence", description: "The quality or state of having sufficient knowledge, judgment, skill, or strength (as for a particular duty or in a particular respect)", price_hour: 10, user: user)
+trait1 = Trait.create!(name: "Warmth", description: "A warm person is friendly towards other people, respecting them and caring for them. With such warmth they create a reciprocal liking, trust and bonding. In contrast a cold person has a lot more difficulty in gaining sympathy from others, and perhaps does not care about this.", price_hour: 15, user: user1)
+trait2 = Trait.create!(name: "Outgoing", description: "The adjective outgoing describes someone with a friendly, easy personality. Your outgoing friend might not understand how hard it is for shy people to speak in front of an audience. If you're the only member of your outgoing family who is reserved, you might feel silent and invisible in their sociable midst.", price_hour: 20, user: user2)
+trait3 = Trait.create!(name: "Honest", description: "Honesty is a facet of moral character that connotes positive and virtuous attributes such as integrity, truthfulness, straightforwardness, including straightforwardness of conduct, along with the absence of lying, cheating, theft, etc.", price_hour: 25, user: user3)
+trait4 = Trait.create!(name: "Trustworthy", description: "A trustworthy person is someone in whom we can place our trust and rest assured that the trust will not be betrayed. Because trust is based on truth, trustworthy people must be truthful.", price_hour: 30, user: user4)
 
 puts "traits created"
 
