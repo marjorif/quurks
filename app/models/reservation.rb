fb81c1136925c4768416cc83d05a198d507d231c
ana sanots <anasoliveiraaa@gmail.com>
2020-10-30 15:20:43 +0000

    done

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,12 +1,9 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :trait
-  validates :start_date, presence: true
   validates :end_date, presence: true
   validates :start_date, presence: true, date: { after_or_equal_to: Proc.new { Date.today }, message: "must be at least #{(Date.today + 1).to_s}" }, on: :create
   validate :end_date_is_after_start_date
-
-
 
   def end_date_is_after_start_date
     return if end_date.blank? || start_date.blank?
