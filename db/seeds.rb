3f93140f84d961efc69ab7b20fa5565b710db0c3
Marjori Fonte <marjorif@live.com>
2020-10-31 13:19:07 +0000

    adding open-uri

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+
 Reservation.destroy_all
 Trait.destroy_all
 User.destroy_all
